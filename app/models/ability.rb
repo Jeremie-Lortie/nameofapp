b7412bbcafe58a287533fc654a309409b1850e7d
Jeremie Lortie <jeremie_lortie_vb@hotmail.com>
2017-02-07 23:31:07 +0100

    6.5 add stripe and button

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,15 +5,16 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
   end
-  #def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+  
+  def initialize(user)
+    #Define abilities for the passed in user here. For example:
+    
+       user ||= User.new # guest user (not logged in)
+       if user.admin?
+         can :manage, :all
+       else
+         can :read, :all
+       end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
@@ -32,5 +33,5 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-  #end
+  end
 end
