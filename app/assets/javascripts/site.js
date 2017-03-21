786916dd499e73049fd06a422ba9d394db586a5e
Jeremie Lortie <jeremie_lortie_vb@hotmail.com>
2017-03-21 20:02:52 +0100

    6.9 add javascript elevate zoom

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,24 @@
+$(document).on('turbolinks:load', function(){
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+});
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+       
+       refreshRating();
+       
+      $('.img-zoom').elevateZoom({
+      	tint:true, 
+      	tintColour:'#F90', 
+      	tintOpacity:0.5,
+      	zoomWindowFadeIn: 500,
+		    zoomWindowFadeOut: 500,
+		    lensFadeIn: 500,
+		    lensFadeOut: 500 
+ 	  });
+ }); 
