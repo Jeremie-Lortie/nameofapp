7c2faa31548125454662917c06de9d7f8b30c02d
Jeremie Lortie <jeremie_lortie_vb@hotmail.com>
2017-03-27 18:55:58 +0200

    fix problem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,28 @@
 class Product < ApplicationRecord
-	validates :name, presence: true
+  validates :name, presence: true
   has_many :orders
   has_many :comments
 
   def highest_rating_comment
   	comments.rating_desc.first
-	end
+  end
 
-	def lowest_rating_comment
-		comments.rating_asc.first
-	end
+  def lowest_rating_comment
+	comments.rating_asc.first
+  end
 
-	def average_rating
-  	comments.average(:rating).to_f
-	end
+  def average_rating
+	comments.average(:rating).to_f
+  end
+
+  def views
+    $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
+  end
+
+  def viewed!
+    $redis.inc("product:#{id}") # this is equivalent to 'INC product:1'
+  end
+
 
 
 end
